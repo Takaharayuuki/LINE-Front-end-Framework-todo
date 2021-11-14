ed535bc0224a31ed224ae2465cd5dbd2eff05f4d
Takaharayuuki <takaharayuuki22@gmail.com>
2021-11-14 23:26:23 +0900

    useFetch 試し

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+import { useFetch } from 'nuxt3/dist/app/composables';
+import { ref } from 'vue';
+const count = ref(0);
+
+const { data }: { data: any } = await useFetch("api/hello?search=girls");
+</script>
 <template>
-  <div>Todoめいん</div>
+  <div>
+    <div>Todo</div>
+    <h2>Counter</h2>
+    {{ count }}
+    <button @click="count++">Push</button>
+    {{ data }}
+  </div>
 </template>
